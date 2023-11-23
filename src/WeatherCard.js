85afd5ca0b6da01e07bd2c574dc14c7e4711259f
Greta Va <greta.vasilevskyte@gmail.com>
2023-11-23 18:34:22 +0100

    Add .env file

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,12 +1,14 @@
 import styles from './App.css';
 
-export default function WeatherCard() {
+export default function WeatherCard({ data }) {
+  // const { name, main, weather } = data;
+
   return (
     <div className={styles.app}>
       <div className={styles.container}>
         <div className={styles.top}>
           <div className={styles.location}>
-            <p>London</p>
+            <h2>London</h2>
           </div>
           <div className={styles.temp}>
             <h1>20°C</h1>
@@ -17,11 +19,11 @@ export default function WeatherCard() {
         </div>
         <div className={styles.bottom}>
           <div className={styles.feelsLike}>
-            Feels like
+            <p>Feels like</p>
             <p>18°C</p>
           </div>
           <div className={styles.wind}>
-            Wind speed
+            <p>Wind speed</p>
             <p>5km/h</p>
           </div>
         </div>
